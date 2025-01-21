ca402a3f0f9598e00ccb7668595460c2bceb84d2
Michel Pupulaire <mpaupulaire4@gmail.com>
2025-01-21 08:51:59 -0500

    save new

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm install -g \
+  @fsouza/prettierd \
+  eslint_d \
+  typescript \
+  typescript-language-server \
+  tailwindcss-language-server \
+  unocss-language-server \
+  yaml-language-server \
+  vscode-langservers-extracted \
+  svelte-language-server
+  # prisma-language-server \
